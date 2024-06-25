e8191caf773b3414ef4b856d794f9f72c9efe32f
Julian Burgos <JBURGOS@MITRE.ORG>
2024-06-25 17:13:18 -0400

    branding set to pull

--- AutomateReportRedaction.ps1
+++ AutomateReportRedaction.ps1
@@ -10,17 +10,43 @@ $newJSONFilePath = '.\updated.json'
 # Write the updated content back to the JSON file
 #$jsonContent | Set-Content -Path $jsonFilePath
 
+$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
+
 # Setting Country, City, BusinessPhone, PostalCode to null
 
-$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
-# Update the properties
-$jsonContent.tenant_details | ForEach-Object {
-    $_.AADAdditionalData.City = $null
-    $_.AADAdditionalData.Country = $null
-    $_.AADAdditionalData.CountryLetterCode = $null
-    $_.AADAdditionalData.State = $null
-    $_.AADAdditionalData.Street = $null
+function Update-Keys {
+    param(
+        [Parameter(Mandatory=$true)]
+        [PSObject]$Object,
+        [Parameter(Mandatory=$true)]
+        [string[]]$Keys
+    )
+    $Object.PSObject.Properties | ForEach-Object {
+        if ($_.Value -is [PSObject]) {
+            Update-Keys -Object $_.Value -Keys $Keys
+        }
+        elseif ($_.Value -is [array]) {
+            if ($_.Name -eq 'BusinessPhones') {
+                $_.Value[0] = "1234567890"
+            } else {
+                $_.Value | ForEach-Object {
+                    if ($_ -is [PSObject]) {
+                        Update-Keys -Object $_ -Keys $Keys
+                    }
+                }
+            }
+        }
+        elseif ($Keys -contains $_.Name) {
+            $_.Value = $null
+        }
+    }
 }
+
+
+# Specify the keys to update
+$keysToUpdate = @('BusinessPhones','City', 'Country', 'CountryLetterCode', 'CountryAbbreviation', 'State', 'Street', 'PostalCode')
+# Update the keys
+Update-Keys -Object $jsonContent -Keys $keysToUpdate
 # Convert the updated object back to JSON
 $jsonContent = $jsonContent | ConvertTo-Json -Depth 20
 # Write the updated content back to the JSON file
