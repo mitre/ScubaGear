a86a96d98d01061699bf91ed5a39fcf865c1b81b
julianjburgos <julianjburgos@gmail.com>
2024-06-26 18:08:11 -0400

    removed all cap table data that don't have policy id

--- AutomateReportRedaction.ps1
+++ AutomateReportRedaction.ps1
@@ -23,39 +23,13 @@
 
 $jsonFilePath = '.\settings.json'
 $newJSONFilePath = '.\updated.json'
+$sampleGUID = 'ca08493a-c9c8-4db0-a9e8-d3b4bafac269'
+$privilegedUsersFilePath = '.\redacted_privileged_users.json'
+$privilegedUsers = Get-Content -Path $privilegedUsersFilePath | ConvertFrom-Json
+
 
 $jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
-# Setting Country, City, BusinessPhone, PostalCode to null
-<#
-function Update-Keys {
-    param(
-        [Parameter(Mandatory=$true)]
-        [PSObject]$Object,
-        [Parameter(Mandatory=$true)]
-        [string[]]$Keys
-    )
-    $Object.PSObject.Properties | ForEach-Object {
-        if ($_.Value -is [PSObject]) {
-            Update-Keys -Object $_.Value -Keys $Keys
-        }
-        elseif ($_.Value -is [array]) {
-            if ($_.Name -eq 'BusinessPhones') {
-                $_.Value[0] = "1234567890"
-            } else {
-                $_.Value | ForEach-Object {
-                    if ($_ -is [PSObject]) {
-                        Update-Keys -Object $_ -Keys $Keys
-                    }
-                }
-            }
-        }
-        elseif ($Keys -contains $_.Name) {
-            $_.Value = $null
-        }
-    }
-}
-#>
 
 function Update-Keys {
     param(
@@ -65,28 +39,35 @@ function Update-Keys {
         [string[]]$Keys
     )
     $Object.PSObject.Properties | ForEach-Object {
-        if ($_.Value -is [PSObject]) {
+        if ($_.Name -eq 'TenantId' -or $_.Name -eq 'tenantId') {
+            $_.Value = $sampleGUID
+        }
+
+        elseif ($_.Name -eq 'privileged_users') {
+            $_.Value = $privilegedUsers
+        }
+
+        elseif ($_.Name -eq 'primaryApprovers') {
+            $_.Value = @(
+                @{
+                    "@odata.type" = "#microsoft.graph.groupMembers"
+                    "isBackup" = $false
+                    "id" = "54e56ffb-a568-4c65-b04a-7a6feabab17c"
+                    "description" = "privileged escalation approvers"
+                }
+            )
+        }
+
+        elseif ($_.Name -eq 'properties' -and $_.Value -is [string]) {
+            $_.Value = $_.Value -replace '(tenantId=)[^;]*', "`$1$sampleGUID"
+        }
+
+        elseif ($_.Value -is [PSObject]) {
             Update-Keys -Object $_.Value -Keys $Keys
         }
         elseif ($_.Value -is [array]) {
             if ($_.Name -eq 'BusinessPhones') {
                 $_.Value[0] = "1234567890"
-            } 
-            #TODO FIX
-            elseif ($_.Name -eq 'VerifiedDomains') {
-                $first = $true
-                for ($i=0; $i -lt $_.Value.Count; $i++) {
-                    if ($_.Value[$i].Name -eq 'tqhjy.onmicrosoft.com' -and $_.Value[$i].Capabilities -match 'Email' -and $_.Value[$i].Capabilities -match 'OfficeCommunicationsOnline') {
-                        if ($first) {
-                            $first = $false
-                        } else {
-                            $_.Value[$i] = $null
-                        }
-                    } else {
-                        $_.Value[$i] = $null
-                    }
-                }
-                $_.Value = $_.Value | Where-Object { $_ -ne $null }
             } 
             elseif ($_.Name -eq 'TechnicalNotificationMails') {
                 $_.Value = @("admin@example.com")
@@ -94,6 +75,17 @@ function Update-Keys {
             elseif ($_.Name -eq 'notificationRecipients' -and $_.Value.Count -gt 0) {
                 $_.Value = @("admin@example.com")
             } 
+
+            elseif ($_.Name -eq 'VerifiedDomains') {
+                $firstVerifiedDomain = $_.Value | Where-Object { $_.Name -eq 'cisaent.onmicrosoft.com' -and $_.Capabilities -match 'Email' -and $_.Capabilities -match 'OfficeCommunicationsOnline' } | Select-Object -First 1
+                if ($firstVerifiedDomain) {
+                    $_.Value = @($firstVerifiedDomain)
+                }
+            }
+            elseif ($_.Name -eq 'cap_table_data' -or $_.Name -eq 'conditional_access_policies') {
+                $_.Value = $_.Value | Where-Object { $_.DisplayName -match '^(Live|MS\.\w+\.\d+\.\d+v\d+)' }
+            }
+
             else {
                 for ($i=0; $i -lt $_.Value.Count; $i++) {
                     if ($_.Value[$i] -is [PSObject]) {
