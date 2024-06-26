60fe4f424c0dea32f8948067375917f1d63b18de
julianjburgos <julianjburgos@gmail.com>
2024-06-26 11:19:35 -0400

    keywords cisaent changed to tqhjy

--- AutomateReportRedaction.ps1
+++ AutomateReportRedaction.ps1
@@ -1,19 +1,33 @@
+<#
+    .SYNOPSIS
+        Script to automate Redaction of Sample Reports
+
+    .DESCRIPTION
+        This script executes on a generated sample report and replaces items like CISA, cisaent, 
+        and any other type of identifiable information with redacted generic information
+
+    .EXAMPLE
+        .\RunUnitTests.ps1
+        Runs every unit test of every product, no flags necessary
+
+    .EXAMPLE
+        .\AutomateReportRedaction
+        
+
+#>
+
+
 # Switching CISA to tqhjy
 # TODO: get the relative path of the JSON file
 
 
 $jsonFilePath = '.\settings.json'
 $newJSONFilePath = '.\updated.json'
-#$jsonContent = Get-Content -Path $jsonFilePath -Raw
-
-#$jsonContent = $jsonContent.Replace('cisaent', 'tqhjy')
-# Write the updated content back to the JSON file
-#$jsonContent | Set-Content -Path $jsonFilePath
 
 $jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
 # Setting Country, City, BusinessPhone, PostalCode to null
-
+<#
 function Update-Keys {
     param(
         [Parameter(Mandatory=$true)]
@@ -41,7 +55,58 @@ function Update-Keys {
         }
     }
 }
+#>
 
+function Update-Keys {
+    param(
+        [Parameter(Mandatory=$true)]
+        [PSObject]$Object,
+        [Parameter(Mandatory=$true)]
+        [string[]]$Keys
+    )
+    $Object.PSObject.Properties | ForEach-Object {
+        if ($_.Value -is [PSObject]) {
+            Update-Keys -Object $_.Value -Keys $Keys
+        }
+        elseif ($_.Value -is [array]) {
+            if ($_.Name -eq 'BusinessPhones') {
+                $_.Value[0] = "1234567890"
+            } else {
+                for ($i=0; $i -lt $_.Value.Count; $i++) {
+                    if ($_.Value[$i] -is [PSObject]) {
+                        Update-Keys -Object $_.Value[$i] -Keys $Keys
+                    }
+                    elseif ($_.Value[$i] -match 'cisaent') {
+                        $_.Value[$i] = $_.Value[$i] -replace 'cisaent', 'tqhjy'
+                    }
+                    elseif ($_.Value[$i] -match 'cisaent.mail') {
+                        $_.Value[$i] = $_.Value[$i] -replace 'cisaent.mail', 'tqhjy'
+                    }
+                }
+            }
+        }
+        elseif ($Keys -contains $_.Name) {
+            $_.Value = $null
+        }
+        
+        elseif ($_.Value -match 'cisaent.mail') {
+            $_.Value = $_.Value -replace 'cisaent.mail', 'tqhjy'
+        }
+        
+        elseif ($_.Value -match 'cisaent') {
+            $_.Value = $_.Value -replace 'cisaent', 'tqhjy'
+        }
+
+        elseif ($_.Value -match 'cisa') {
+            $_.Value = $_.Value -replace 'cisa', 'tqhjy'
+        }
+
+        <#elseif ($_.Value -match 'cisa') {
+            $_.Value = $_.Value -replace 'cisa', 'tqhjy'
+        }#>
+        
+    }
+}
 
 # Specify the keys to update
 $keysToUpdate = @('BusinessPhones','City', 'Country', 'CountryLetterCode', 'CountryAbbreviation', 'State', 'Street', 'PostalCode')
