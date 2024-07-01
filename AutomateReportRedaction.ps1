599292bb394ce69bf1d6fcc5d700a5afcaf9fe27
julianjburgos <julianjburgos@gmail.com>
2024-07-01 10:37:51 -0400

    WIP

--- AutomateReportRedaction.ps1
+++ AutomateReportRedaction.ps1
@@ -23,12 +23,14 @@
 
 $jsonFilePath = '.\settings.json'
 $newJSONFilePath = '.\updated.json'
+#TODO make constants
 $sampleGUID = 'ca08493a-c9c8-4db0-a9e8-d3b4bafac269'
 $privilegedUsersFilePath = '.\redacted_privileged_users.json'
 $privilegedUsers = Get-Content -Path $privilegedUsersFilePath | ConvertFrom-Json
+$usStateAbbreviations = @("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "DC")
 
 
-$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
+#$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
 
 function Update-Keys {
@@ -41,6 +43,10 @@ function Update-Keys {
     $Object.PSObject.Properties | ForEach-Object {
         if ($_.Name -eq 'TenantId' -or $_.Name -eq 'tenantId') {
             $_.Value = $sampleGUID
+        }
+
+        elseif ($_.Name -eq 'State' -or $_.Name -eq 'StateOrProvince' -and $usStateAbbreviations -contains $_.Value) {
+            $_.Value = $null
         }
 
         elseif ($_.Name -eq 'privileged_users') {
@@ -56,6 +62,10 @@ function Update-Keys {
                     "description" = "privileged escalation approvers"
                 }
             )
+        }
+
+        elseif ($_.Name -eq 'domain_settings') {
+            $_.Value = $_.Value | Where-Object { $_.Id -eq 'tqhjy.onmicrosoft.com' }
         }
 
         elseif ($_.Name -eq 'properties' -and $_.Value -is [string]) {
@@ -77,7 +87,7 @@ function Update-Keys {
             } 
 
             elseif ($_.Name -eq 'VerifiedDomains') {
-                $firstVerifiedDomain = $_.Value | Where-Object { $_.Name -eq 'cisaent.onmicrosoft.com' -and $_.Capabilities -match 'Email' -and $_.Capabilities -match 'OfficeCommunicationsOnline' } | Select-Object -First 1
+                $firstVerifiedDomain = $_.Value | Where-Object { $_.Name -eq 'tqhjy.onmicrosoft.com' -and $_.Capabilities -match 'Email' -and $_.Capabilities -match 'OfficeCommunicationsOnline' } | Select-Object -First 1
                 if ($firstVerifiedDomain) {
                     $_.Value = @($firstVerifiedDomain)
                 }
@@ -100,15 +110,30 @@ function Update-Keys {
     }
 }
 
-# Specify the keys to update
-$keysToUpdate = @('BusinessPhones','City', 'Country', 'CountryLetterCode', 'CountryAbbreviation', 'Street', 'PostalCode')
 
-# Update the keys
-Update-Keys -Object $jsonContent -Keys $keysToUpdate
+# Load the JSON content
+$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
+ 
+# Convert the JSON content to a string
 $jsonString = $jsonContent | ConvertTo-Json -Depth 20
+ 
+# Perform the string replacements
+# TODO: replace this with arguments, make tqhjy a constant variable
 $jsonString = $jsonString -replace 'Cybersecurity and Infrastructure Security Agency', 'tqhjy'
 $jsonString = $jsonString -replace 'cisaent.mail', 'tqhjy'
 $jsonString = $jsonString -replace 'cisaent', 'tqhjy'
-#$jsonString = $jsonString -replace 'cisa', 'SCuBA'
+ 
+# Convert the string back to JSON
 $jsonContent = $jsonString | ConvertFrom-Json
-$jsonContent | ConvertTo-Json -Depth 20 | Set-Content -Path $newJSONFilePath
+ 
+# Specify the keys to update
+$keysToUpdate = @('BusinessPhones','City', 'Country', 'CountryLetterCode', 'CountryAbbreviation', 'Street', 'PostalCode')
+ 
+# Update the keys
+Update-Keys -Object $jsonContent -Keys $keysToUpdate
+ 
+# Convert the updated JSON content back to a string
+$jsonString = $jsonContent | ConvertTo-Json -Depth 20
+ 
+# Save the redacted JSON data to a new file
+$jsonString | Set-Content -Path $newJSONFilePath
